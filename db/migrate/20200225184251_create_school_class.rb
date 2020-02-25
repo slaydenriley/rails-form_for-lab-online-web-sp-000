ea84137ab2b72e111ed71f5044555356bc47621e
Riley Slayden <slaydenriley@gmail.com>
2020-02-25 19:02:20 +0000

    Automatically backed up by Learn

--- db/migrate/20200225184251_create_school_class.rb
+++ db/migrate/20200225184251_create_school_class.rb
@@ -1,6 +1,6 @@
-class CreateSchoolClass < ActiveRecord::Migration[5.0]
+class CreateSchoolClasses < ActiveRecord::Migration[5.0]
   def change
-    create_table :school_classes do |t|
+    create_table :school_class do |t|
       t.string :title
       t.integer :room_number
 
