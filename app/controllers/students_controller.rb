77800405222c46f9787e3bdd8e8c347434f862ab
Riley Slayden <slaydenriley@gmail.com>
2020-02-25 18:57:38 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,31 @@
 class StudentsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
   end
 
   def edit
+    @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit!
   end
 end
